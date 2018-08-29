688fd50b7b195785f8cd03e2e74615e0b0dd5646
Ben Sartor <mail@ben-sartor.de>
2018-08-29 20:37:28 +0200

    move destination dir

--- extract-vendor-image.sh
+++ extract-vendor-image.sh
@@ -4,10 +4,6 @@ set -eu -o pipefail
 declare -r VENDOR_IMAGE_ID=${1:?"usage example: $0 opm6.171019.030.H1"}
 
 
-declare -r DEST_DIR="vendor-image-${VENDOR_IMAGE_ID}"
-mkdir "${DEST_DIR}"
-
-
 declare -r TEMP_DIR=$(mktemp --directory --tmpdir extract-vendor-image-XXXXXXXXXX)
 echo "created temporary directory: ${TEMP_DIR}"
 
@@ -17,3 +13,7 @@ function cleanup {
 }
 trap cleanup EXIT
 
+
+declare -r DEST_DIR="vendor-image-${VENDOR_IMAGE_ID}"
+mkdir "${DEST_DIR}"
+echo "created destination directory: ${DEST_DIR}"
