be18b75b18512b4d4cd6d37d57e91b14a583275b
Ben Sartor <mail@ben-sartor.de>
2018-08-29 20:35:52 +0200

    init directories

---
+++ extract-vendor-image.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -eu -o pipefail
+
+declare -r VENDOR_IMAGE_ID=${1:?"usage example: $0 opm6.171019.030.H1"}
+
+
+declare -r DEST_DIR="vendor-image-${VENDOR_IMAGE_ID}"
+mkdir "${DEST_DIR}"
+
+
+declare -r TEMP_DIR=$(mktemp --directory --tmpdir extract-vendor-image-XXXXXXXXXX)
+echo "created temporary directory: ${TEMP_DIR}"
+
+function cleanup {
+    echo "removing temporary directory: ${TEMP_DIR}"
+    rm -rf "${TEMP_DIR}"
+}
+trap cleanup EXIT
+
